4e8edfa8ad50939445fe4535d5ff070f1aa416c8
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-05-08 14:04:20 +0530

    i kinda like the thinking process :)

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
-from src.tower import Tower
+from src.tower import Tower, Organizer
 
-'''import pygame
-import sys
+# import pygame
+# import sys
 
 bg_color = (255, 255, 255)
 width, height = 1080, 720
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("JAK Tower of Hanoi Game")
-screen.fill(bg_color)
-
-pygame.display.flip()
-
 game_loop = True
-while game_loop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_loop = False
 
-pygame.quit()
-sys.exit()'''
-
-
-tower1 = Tower(["0", "1", "2"])
+tower1 = Tower(["Big", "Mid", "Small"])
 tower2 = Tower([])
 tower3 = Tower([])
 
-organizer = {"1": tower1, "2": tower2, "3": tower3}
+organizer = Organizer(tower1=tower1, tower2=tower2, tower3=tower3)
+
+# screen = pygame.display.set_mode((width, height))
+# pygame.display.set_caption("JAK Tower of Hanoi Game")
+# screen.fill(bg_color)
+
+# clock = pygame.time.Clock()
 
 
 def get_operation(operator):
     towers = operator.strip().split("->")
+
     remove_tower = organizer.get(towers[0])
     add_tower = organizer.get(towers[1])
+
+    if remove_tower == add_tower:
+        raise Exception()
+
+    # TODO: Check for Larger on top of Small problem
 
     popped_ring = remove_tower.pop()
     add_tower.push(popped_ring)
 
 
-while True:
+while game_loop:
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         game_loop = False
+
     try:
         print(f"Tower 1: {tower1.rings}")
         print(f"Tower 2: {tower2.rings}")
         print(f"Tower 3: {tower3.rings}")
+        print()
 
-        get_operation(str(input(">> ")))
+        if tower3.rings == ["Big", "Mid", "Small"]:
+            print("You win!")
+            game_loop = False
+        else:
+            get_operation(str(input(">> ")))
+            print()
     except Exception:
         print("ばか がいじん")
+        print()
+
+    # pygame.display.flip()
+    # clock.tick(60)
+
+# pygame.quit()
+# sys.exit()
--- src/tower.py
+++ src/tower.py
@@ -10,32 +10,15 @@ class Tower:
     def pop(self) -> str:
         if self.top is None:
             raise Exception()
+
         self.top = None if self.top == 0 else self.top - 1
+
         return self.rings.pop()
 
 
-tower1 = Tower(["0", "1", "2"])
-tower2 = Tower([])
-tower3 = Tower([])
+class Organizer:
+    def __init__(self, **towers):
+        self.towers = towers
 
-organizer = {"1": tower1, "2": tower2, "3": tower3}
-
-
-def get_operation(operator):
-    towers = operator.strip().split("->")
-    remove_tower = organizer.get(towers[0])
-    add_tower = organizer.get(towers[1])
-
-    popped_ring = remove_tower.pop()
-    add_tower.push(popped_ring)
-
-
-while True:
-    try:
-        print(f"Tower 1: {tower1.rings}")
-        print(f"Tower 2: {tower2.rings}")
-        print(f"Tower 3: {tower3.rings}")
-
-        get_operation(str(input(">> ")))
-    except Exception:
-        print("ばか がいじん")
+    def get(self, tower_id: str):
+        return self.towers[f"tower{tower_id}"]
